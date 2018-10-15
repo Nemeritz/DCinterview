f35b2bf0b6f187cd20f700cc4fb470fbeccc5bcc
Hanliang Ding <hdin898@aucklanduni.ac.nz>
2018-10-16 10:15:46 +1300

    broken tests

--- src/test_Case.py
+++ src/test_Case.py
@@ -5,5 +5,4 @@ import CLoader
 class AddTest(unittest.TestCase):
 	def test_addition(self):
 		module = CLoader.load('example')
-		self.assertEqual(module.add(1, 2), 1 + 2)
-
+		self.assertEqual(module.add(1, 2), 1 + 3)
