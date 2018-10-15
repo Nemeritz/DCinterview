eafbe8e9ce63c11af50987c0a83be525119d7154
Hanliang Ding <hdin898@aucklanduni.ac.nz>
2018-10-16 08:00:09 +1300

    Another error in test

--- src/test_Case.py
+++ src/test_Case.py
@@ -5,5 +5,5 @@ import CLoader
 class AddTest(unittest.TestCase):
 	def test_addition(self):
 		module = CLoader.load('example')
-		self.assertEqual(module.add(1, 2), 1 + 3)
+		self.assertEqual(module.add(1, 2), 1 * 2)
 
